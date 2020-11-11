89a5a141334b7572b12e8ce74169c199c35a06d2
Jumpybuns <ethanpiercepresents@gmail.com>
2020-11-11 10:18:24 -0800

    test2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -35,31 +35,94 @@ describe('app routes', () => {
 
       const expectation = [
         {
-          'id': 1,
+          'id': 4,
           'todo': 'sweep',
-          'completed': true,
-          'owner_id': 1
+          'completed': false,
+          'owner_id': 2
         },
         {
-          'id': 2,
+          'id': 5,
           'todo': 'dust',
           'completed': false,
-          'owner_id': 1
+          'owner_id': 2
         },
         {
-          'id': 3,
+          'id': 6,
           'todo': 'clean',
-          'completed': true,
-          'owner_id': 1
+          'completed': false,
+          'owner_id': 2
         }
       ];
 
+      await fakeRequest(app)
+        .post('/api/todos')
+        .send(expectation[0])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      await fakeRequest(app)
+        .post('/api/todos')
+        .send(expectation[1])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      await fakeRequest(app)
+        .post('/api/todos')
+        .send(expectation[2])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
       const data = await fakeRequest(app)
         .get('/api/todos')
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
     });
+    test('add one todo', async() => {
+
+      const expectation = [{
+        'id': 4,
+        'todo': 'walk the dog',
+        'completed': false,
+        'owner_id': 2
+      }];
+
+      const data = await fakeRequest(app)
+        .post('/api/todos')
+        .send({
+          'todo': 'walk the dog',
+          'completed': false,
+        })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test.only('updates todo', async() => {
+
+      const expectation = [{
+        'id': 4,
+        'todo': 'walk the dog',
+        'completed': true,
+        'owner_id': 2
+      }];
+
+      const data = await fakeRequest(app)
+        .put('/api/todos/7')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+
   });
 });
